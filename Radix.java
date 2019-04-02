1a7e7a49fd91b04de115c208b6b5612139a97538
Tiffany Cao <tcao00@stuy.edu>
2019-04-02 00:07:38 -0400

    temporary radixsort

--- Radix.java
+++ Radix.java
@@ -2,19 +2,27 @@ import java.util.*;
 
 public class Radix{
   public static void radixsort(int[] data){
-    boolean done = false;
     int place = 10;
-    while(!done){
-      MyLinkedList<MyLinkedList<Integer>> buckets = new MyLinkedList<MyLinkedList<Integer>>();
-      for(int i = 0; i < data.length; i++){
-        int temp = data[i] % place;
-        if(data[i] < 0){
-          MyLinkedList<Integer> list = buckets.getNode(i);
-          list.add(data[i]);
-        }else{
-          buckets[10 + temp] = data[i];
-        }
+    MyLinkedList<Integer>[] buckets = new MyLinkedList[19];
+    for(int i = 0; i < buckets.length; i++){
+      buckets[i] = new MyLinkedList<Integer>();
+    }
+    for(int i = 0; i < data.length; i++){
+      int temp = data[i] % place;
+      if(data[i] < 0){
+        MyLinkedList<Integer> list = buckets[9 - temp];
+        list.add(data[i]);
+      }else{
+        MyLinkedList<Integer> list = buckets[10 + temp];
+        list.add(data[i]);
       }
     }
+    MyLinkedList<Integer> newData = buckets[0];
+    for(int i = 1; i < buckets.length; i++){
+      if(buckets[i].size() != 0){
+        newData.extend(buckets[i]);
+      }
+    }
+    radixHelper(newData);
   }
 }
