62b1fa21ee614299912b1d7a1682158c9137d48d
Tiffany Cao <tcao00@stuy.edu>
2019-04-03 21:40:27 -0400

    fixed mistake, tested larger case

--- Radix.java
+++ Radix.java
@@ -2,39 +2,44 @@ import java.util.*;
 
 public class Radix{
 
+  /**A method that sorts an array of integers using the radix sort algorithm
+  *@param int[] data
+  */
   @SuppressWarnings("unchecked")
   public static void radixsort(int[] data){
-    //System.out.println(toString(data));
+    //find the largest number
     int largest = 0;
     for(int i = 0; i < data.length; i++){
-      if(Math.abs(data[i]) > 0) largest = Math.abs(data[i]);
+      if(Math.abs(data[i]) > largest) largest = Math.abs(data[i]);
     }
-    //System.out.println(largest);
+    //System.out.println("largest: " + largest);
+    //find the number of digits in the largest number
     int numDigits = 0;
     while(largest > 0){
       numDigits++;
       largest = largest / 10;
     }
-    //System.out.println(numDigits);
+    //System.out.println("numDigits: " + numDigits);
 
     int place = 10;
-    MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
+    MyLinkedList<Integer>[] buckets = new MyLinkedList[20]; //creating buckets
     for(int i = 0; i < buckets.length; i++){
       buckets[i] = new MyLinkedList<Integer>();
     }
+    //first round of sorting
     for(int i = 0; i < data.length; i++){
-      int temp = Math.abs(data[i] % place);
+      int temp = Math.abs(data[i] % place); //finding the digit at the ones place
       //System.out.println("temp@" + i + ": " + temp);
-      if(data[i] < 0){
-        MyLinkedList<Integer> list = buckets[9 - temp];
-        list.add(data[i]);
+      if(data[i] < 0){ //negative
+        buckets[9 - temp].add(data[i]);
         //System.out.println("list@" + (9-temp) + ": " + list);
-      }else{
-        MyLinkedList<Integer> list = buckets[10 + temp];
-        list.add(data[i]);
+      }else{ //positive
+        buckets[10 + temp].add(data[i]);
         //System.out.println("list@" + (10+temp) + ": " + list);
       }
     }
+
+    //linking the bucket lists
     MyLinkedList<Integer> first = buckets[0];
     for(int i = 1; i < buckets.length; i++){
       if(buckets[i].size() != 0){
@@ -42,56 +47,50 @@ public class Radix{
       }
     }
     //System.out.println("first: " + first);
-    numDigits--;
+    numDigits--; //the number of digits left to check decreases
 
-    place = place * 10;
+    place = place * 10; //increase the digit place
     while(numDigits != 0){
       //System.out.println("place: " + place);
-      buckets = new MyLinkedList[20];
+      buckets = new MyLinkedList[20]; //new buckets made
       for(int i = 0; i < buckets.length; i++){
         buckets[i] = new MyLinkedList<Integer>();
       }
-      while(first.hasNext()){
-        int temp = first.next();
-        int toAdd = Math.abs((temp % place) / (place / 10));
-        if(temp < 0){
+      while(first.hasNext()){ //iterator
+        int temp = first.next(); //storage for the number
+        int toAdd = Math.abs((temp % place) / (place / 10)); //find the digit
+        if(temp < 0){ //negative
           buckets[9 - toAdd].add(temp);
-        }else{
+        }else{ //positive
           buckets[10 + toAdd].add(temp);
         }
       }
-      /*
-      for(int i = 0; i < first.size(); i++){
-        int temp = Math.abs((first.get(i) % place) / (place / 10));
-        //System.out.println("temp@" + i + ": " + temp);
-        if(first.get(i) < 0){
-          MyLinkedList<Integer> list = buckets[9 - temp];
-          list.add(first.get(i));
-          //System.out.println("list@" + (9-temp) + ": " + list);
-        }else{
-          MyLinkedList<Integer> list = buckets[10 + temp];
-          list.add(first.get(i));
-          //System.out.println("list@" + (10+temp) + ": " + list);
-        }
-      }
-      */
-      place = place * 10;
+
+      place = place * 10; //increase the digit place
+      //linking the bucket lists
       MyLinkedList<Integer> newData = buckets[0];
       for(int i = 1; i < buckets.length; i++){
         if(buckets[i].size() != 0){
           newData.extend(buckets[i]);
         }
       }
-      first = newData;
+      first = newData; //set the new list to first
       //System.out.println("first: " + first);
-      numDigits--;
+      numDigits--; //the number of digits left to check decreases
     }
+    //System.out.println(first);
 
-    for(int i = 0; i < first.size(); i++){
-      data[i] = first.get(i);
+    int index = 0; //copying over to the original array
+    while(first.hasNext()){
+      data[index] = first.next();
+      index++;
     }
   }
 
+  /**A method that prints out an array
+  *@param int[] data
+  *@return String
+  */
   public static String toString(int[] data){
     String result = "[";
     for(int i = 0; i < data.length; i++){
@@ -111,5 +110,13 @@ public class Radix{
     System.out.println(toString(data1));
     radixsort(data1);
     System.out.println(toString(data1));
+
+    int[] data3 = new int[1000];
+    for(int i = 999; i >= 0; i--){
+      data3[999-i] = i;
+    }
+    System.out.println(toString(data3));
+    radixsort(data3);
+    System.out.println(toString(data3));
   }
 }
